e2f9241af15dc6465db9c05a4f505b8a5465196b
SamantaX-31 <samanta.shuvajit2005@gmail.com>
2024-07-04 10:08:14 +0530

    Updated

--- C programming/Age Calculator.c
+++ C programming/Age Calculator.c
@@ -2,8 +2,8 @@
 #include<math.h>
 int main()
 {
-    int d1,m1,y1,PD;
-    int d2,m2,y2,BD;
+    int d1,m1,y1;
+    int d2,m2,y2;
     int D,M,Y;
 
 
@@ -14,21 +14,54 @@ int main()
     scanf("%d/%d/%d", &d1,&m1,&y1);
 
     
-    if((d1>=d2 && m1>=m2) || d1>d2 || m1>m2)
+    //age calculate in years
+    
+        Y=y1-y2;
+    
+    
+    //age calculate in months
+    
+    if(m1<=m2)
+    {
+        Y--;
+        M=(12+m1)-m2;
+    }
+    else
+    {
+        M=m1-m2;
+    }
+    //age calculate in days
+    if(d1<=d2)
+    {
+        M--;
+        if(m1==1 || m1==3 || m1==5 || m1==7 
+        || m1==8 || m1==10 || m1==12)
+        {
+            D=(31+d1)-d2;
+        } 
+        
+        else if(m1==4 || m1==6 || m1==9 || m1==11)
+        {
+            D=(30+d1)-d2;
+        }
+        else
+        {
+            if((y1%4==0 && y1%100!=0)|| y1%400==0)
+            {
+                D=(29+d1)-d2;
+            }
+            else
+            {
+                D=(28+d1)-d2;
+            }
+        }
+    }
+    
+    else
     {
         D=d1-d2;
-        M=m1-m2;
-        Y=y1-y2;
     }
-
-    else if((d1<=d2 && m1<=m2) || d1<d2 || m1<m2)
-    {
-        D=(30+d1)-d2;
-        M=(11+m1)-m2;
-        Y=(y1-1)-y2;
-    }
-
     printf("Your Age is %d years %d months %d days",Y,M,D);
-    
+    //done
     return 0;  
 }
---
+++ C programming/Size_of_operator.c
@@ -0,0 +1,13 @@
+// size of oparator 
+#include<stdio.h>
+int main(void)
+{
+    printf("On my system\n");
+    printf("Int is:", sizeof(int));
+    printf("Float is:", sizeof(float));
+    printf("Char is:", sizeof(char));
+    printf("Long is:", sizeof(long));
+    printf("Double is:", sizeof(double));
+    //done......
+    return 0;
+}
